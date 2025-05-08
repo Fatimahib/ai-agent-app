93a1f38708c96f4373ca1eadc6e3f35224e81ea5
Fatimahib <fatm499@hotmail.com>
2025-05-09 01:31:12 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { AuthProvider } from './context/Authcontext'
+import { AuthProvider } from './context/AuthContext'
 import { AppProvider } from './context/AppContext'
 import './index.css'
 
